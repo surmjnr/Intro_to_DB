c1a03e33023ae37757f9b18625976619e54c3e4d
Banafo <surmjnr@gmail.com>
2025-10-19 21:24:57 +0000

    Correct foreign key reference case sensitivity in alx_book_store.sql and task_2.sql; add USE statement in task_3.sql for database context.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create Order_Details table
@@ -45,6 +45,6 @@ CREATE TABLE IF NOT EXISTS order_details (
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,6 @@
 -- List all the tables created
 -- Script to list all tables in alx_book_store database
 
+USE alx_book_store;
+
 SHOW TABLES;
